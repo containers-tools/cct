802a11f2f1e61698eb1115b5e72d891cad86ed9c
David Becvarik <dbecvari@redhat.com>
2015-06-01 18:56:42 +0200

    moved xmledit to use our xmlutils

--- cct/modules/xmledit/cct_module.py
+++ cct/modules/xmledit/cct_module.py
@@ -9,32 +9,19 @@ of the BSD license. See the LICENSE file for details.
 import logging
 import sys
 import xml.etree.ElementTree as ET
-from lxml import etree
 
+from lxml import etree
+from cct.lib.xmlutils import add_element
 from cct.module import Module
 logger = logging.getLogger('cct')
 
 class XML(Module):
 
-    def run(self, operation):
-        if operation.command == "add_element":
-            self.add_element(operation.args)
-        else:
-            logger.info("unsuported operation %s with args %s" % (operation.command, operation.args ))
-
-    def add_element(self, args):
+    def insert_element(self, args):
         xmlfile = args[0]
         xpath = args[1]
-        snipplet = args[2]
-        xmlsniplet = etree.fromstring(snipplet)
-        tree = etree.parse(xmlfile)
-        root = tree.getroot()
-        result = root.xpath(xpath)
-        for element in result:
-            element.append(xmlsniplet)
-        print etree.tostring(tree)
-        
-        
+        snippet = args[2]
+        add_element(xmlfile, xpath, snippet)
 
         
 
