30afe91f87262528380c9ad9ac7f009c3ca00549
David Becvarik <dbecvari@redhat.com>
2015-07-22 11:52:11 +0200

    implemented proper xmledit  module

--- cct/modules/xmledit/cct_module.py
+++ cct/modules/xmledit/cct_module.py
@@ -19,10 +19,63 @@ class XML(Module):
     xmledit = None
 
     def setup(self, xmlfile, *namespaces):
+        """
+        setups xmledit module
+
+        Args:
+            xmlfile: path to an xml file
+            namespaces: list of namespaces in xml file (optional - namespaces are detected automatically and default namespace name is set to "ns" for xpath purposes
+        """   
         self.xmledit = XMLEdit(xmlfile, namespaces)
 
     def insert(self, xpath, snippet):
+        """
+        inserts xml snippet into xpath location
+
+        Args:
+            xpath: xpath to locate place for inserting snippet
+            snippet: xml snippet to insert
+        """
         self.xmledit.add_element(xpath, snippet)
 
+    def delete(self, xpath):
+        """
+        deletes element and all of its child from an xml file
 
-    
+        Args:
+           xpath: xpath to locate an element to be removed
+        """
+        self.xmledit.delete_element(xpath)
+
+    def replace_attribute(self, xpath, attrib, value):
+        """
+        replaces/defines an attribute in element by defined value
+
+        Args:
+            xpath: xpath to locate element for attribute update
+            attrib: name of attribute to define/replace
+            value: value for attribute
+        """
+        self.xmledit.update_attrib(xpath, attrib, value)
+
+    def replace_attribute_regex(self, xpath, attrib, pattern, value):
+        """
+        updates attribute with regex pattern/value combo
+
+        Args:
+            xpath: xpath to locate element for attribute update
+            attrib: name of attribute to define/replace
+            pattern: pattern to locate part of the attribute value to replace
+            value: value for regex
+        """
+
+        self.xmledit.update_regex(xpath, attrib, pattern, value)
+
+    def exist(self, xpath):
+        """
+        checks if element exists
+
+        Args:
+            xpath: xpath expression
+        """
+        self.xmledit.exists(xpath)
