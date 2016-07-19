affe71c2abafbf23f3d87cf8136c54741ab7234f
David Becvarik <dbecvari@redhat.com>
2016-07-19 09:58:56 +0200

    FIXED license in files headers

--- cct/module.py
+++ cct/module.py
@@ -3,7 +3,7 @@ Copyright (c) 2015 Red Hat, Inc
 All rights reserved.
 
 This software may be modified and distributed under the terms
-of the BSD license. See the LICENSE file for details.
+of the MIT license. See the LICENSE file for details.
 """
 import imp
 import inspect
