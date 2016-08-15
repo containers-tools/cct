99e5d694661029006e694656716c1ac1c9d93260
David Becvarik <dbecvari@redhat.com>
2016-08-15 10:12:38 +0200

    Added getenv(name) method to cct Module class
    this method enables you to use cct and environment variables via same
    API
    environment variables from host overrides cct ones, you can use it in a
    way:
    
    from cct.module import Module
    
    dummy = Module("dummy", None)
    dummy.getenv("foo")

--- tests/test_unit_module.py
+++ tests/test_unit_module.py
@@ -3,6 +3,7 @@ import os
 import unittest
 
 from cct.module import Modules
+from cct.module import Module
 
 class TestModules(unittest.TestCase):
 
@@ -10,3 +11,24 @@ class TestModules(unittest.TestCase):
         modules = Modules()
         path = os.path.abspath(os.path.dirname(cct.module.__file__)) + "/modules"
         modules.find_modules(path)
+
+    def test_module_getenv_none(self):
+        dummy = Module("dummy", None)
+        self.assertIsNone(dummy.getenv("bar"))
+
+    def test_module_getenv(self):
+        dummy = Module("dummy", None)
+        dummy.environment = { "foo": "foovalue"}
+        self.assertEquals(dummy.getenv("foo"), "foovalue")
+
+    def test_module_getenv_form_host(self):
+        dummy = Module("dummy", None)
+        os.environ['foo'] = "foovalue"
+        self.assertEquals(dummy.getenv("foo"), "foovalue")
+
+    def test_module_getenv_override(self):
+        dummy = Module("dummy", None)
+        dummy.environment = { "foohost": "barvalue"}
+        os.environ['foohost'] = "foovalue"
+        self.assertEquals(dummy.getenv("foohost"), "foovalue")
+
