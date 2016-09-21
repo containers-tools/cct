cff1a681cd049f984404fa2c34b715fb7268784d
David Becvarik <dbecvari@redhat.com>
2016-09-21 16:16:43 +0200

    fixed module scanner to ignore test files
    now all dirs which startswith /test and /. are ignored and py files
    are not loaded from such paths

--- cct/module.py
+++ cct/module.py
@@ -232,6 +232,9 @@ class Modules(object):
         """
         logger.debug("discovering modules in %s" %directory)
         for root, _, files in os.walk(directory):
+            candidate_dir = root.replace(directory, "", 1)
+            if candidate_dir.startswith("/tests") or candidate_dir.startswith("/."):
+                continue
             for candidate in files:
                 if os.path.splitext(candidate)[1] == '.py':
                     logger.debug("inspecting %s" %root + "/" + candidate)
