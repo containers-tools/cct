fd3e4ffd9130499c3731b23c8571c02c64f5eff0
David Becvarik <dbecvari@redhat.com>
2017-07-27 15:46:07 +0200

    removed schema checking

--- cct/cli/main.py
+++ cct/cli/main.py
@@ -19,8 +19,6 @@ from cct.change_processor import ChangeProcessor
 from cct.module import ModuleManager
 
 from urlparse import urlparse
-from pykwalify.core import Core
-from pykwalify.errors import SchemaError
 
 logger = logging.getLogger('cct')
 
@@ -64,7 +62,6 @@ class CCT_CLI(object):
         return yaml.load(stream)
 
     def process_changes(self, changes, modules_dir, artifacts_dir):
-        schema = yaml.safe_load(pkgutil.get_data('cct', 'schema.yaml'))
         for change in changes:
             if change is '':
                 continue
@@ -73,13 +70,6 @@ class CCT_CLI(object):
                 change = self.process_url(change)
             else:
                 change = self.process_file(change)
-
-            c = Core(source_data=change, schema_data=schema)
-            try:
-                c.validate(raise_exception=True)
-            except SchemaError as e:
-                logger.error('CCT change viloates schema, %s', e)
-                raise e
             cp = ChangeProcessor(change, modules_dir, artifacts_dir)
             return cp.process()
 
